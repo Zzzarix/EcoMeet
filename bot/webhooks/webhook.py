6bab1b407aa8030e146895b1babf931a74988314
Zzzarix <android1vvv@gmail.com>
2023-03-30 00:04:38 +1200

    fix

--- bot/webhooks/webhook.py
+++ bot/webhooks/webhook.py
@@ -1,10 +1,10 @@
 from ..config import config
 
-WEBAPP_HOST = '0.0.0.0'
+WEBAPP_HOST = '188.225.83.42'
 WEBAPP_PORT = 8443
 
 WEBHOOK_HOST = f'https://188.225.83.42:8443'
-WEBHOOK_PATH = f"/{config['bot']['token']}"
+WEBHOOK_PATH = f"/ecoMeet/webhook/{config['bot']['token']}"
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 import pathlib, os
